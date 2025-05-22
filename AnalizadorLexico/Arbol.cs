52b7f7f01da99767d0182c48af5aeb8cc280a943
Manuel Maquin <manuelzuniga005@gmail.com>
2025-05-22 09:51:44 -0600

    Comenzando Parte Sintáctica

---
+++ AnalizadorLexico/Arbol.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace AnalizadorLexico
+{
+    public class Arbol
+    {
+        public string Nombre { get; set; }
+        public List<Arbol> Hijos { get; set; }
+
+        public Arbol(string nombre)
+        {
+            Nombre = nombre;
+            Hijos = new List<Arbol>();
+        }
+
+        public TreeNode ToTreeNode()
+        {
+            TreeNode nodo = new TreeNode(Nombre);
+            foreach (var hijo in Hijos)
+            {
+                nodo.Nodes.Add(hijo.ToTreeNode());
+            }
+            return nodo;
+        }
+    }
+}
